64c87f2d2c85b8f8038b14e0ab202b83c9703f44
weirdbloom <its.weirdbloom@gmail.com>
2026-04-01 01:17:14 +0300

    'upd'

--- shader.js
+++ shader.js
@@ -1,78 +1,65 @@
 import * as THREE from "three";
-import {
-    EffectComposer,RenderPass,BrightnessContrastEffect,EffectPass
-} from "postprocessing";
 import { OBJLoader } from "three/addons";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-// --- config & setup ---
+// config & setup
 const canvas = document.querySelector('.webgl');
 const scene = new THREE.Scene();
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight / 2
-};
 
-// --- core objects ---
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.set(-2, 1, 4);
+// renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias:true});
 
-const renderer = new THREE.WebGLRenderer({canvas, antialias:true});
-const composer = new EffectComposer(renderer);
+// camera
+const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.001, 100);
+camera.position.set(-2, 1, 1);
 
-// --- camera ---
+// controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 controls.dampingFactor = 0.05;
 controls.enableZoom = false;
 
-// --- material ---
-const material = new THREE.MeshDepthMaterial({ wireframe: true });
+// material
+const material = new THREE.MeshBasicMaterial({
+    wireframe: true, color: 0x000000 });
 
-// --- obj ---
+// obj loading
 const loader = new OBJLoader();
 const object = await loader.loadAsync('./model-3-lod-0.obj');
 
-object.traverse((child) => {
-    if (child.isMesh) child.material = material;
-});
-object.scale.set(.25, .25, .25);
-object.rotateOnAxis(new THREE.Vector3(1, 0, -1), Math.PI / 2);
+object.traverse(child => child.isMesh && (child.material = material));
+object.scale.set(.25,.25,.25);
+object.rotateOnAxis(new THREE.Vector3(1, 0, -1).normalize(), Math.PI / 2);
 
-// --- scene ---
-scene.add(camera, object);
+scene.add(object);
 
-// --- post ---
-composer.addPass(new RenderPass(scene, camera));
-composer.addPass(
-    new EffectPass(
-        camera,
-        new BrightnessContrastEffect({ contrast: 0.25 })
-    )
-);
+//fog
+scene.background = new THREE.Color(0x000000);
+scene.fog = new THREE.Fog(15659506, 1, 10);
 
-// --- responsiveness ---
+// responsiveness
 const updateSize = () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight / 2;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
 
-    camera.aspect = sizes.width / sizes.height;
+    camera.aspect = width / height;
     camera.updateProjectionMatrix();
 
-    renderer.setSize(sizes.width, sizes.height);
+    renderer.setSize(width, height, false);
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    composer.setSize(sizes.width, sizes.height);
 };
 window.addEventListener('resize', updateSize);
 updateSize();
 
-// --- loop ---
+// loop
 const animate = () => {
     object.rotation.x += 0.0001;
     object.rotation.y += 0.0001;
 
     controls.update();
-    composer.render();
+    renderer.render(scene, camera);
     requestAnimationFrame(animate);
 };
 animate();
